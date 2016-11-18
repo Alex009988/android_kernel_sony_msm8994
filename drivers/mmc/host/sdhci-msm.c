b256340e20d3f7a641d187a9aa00e473db63a43b
Sayali Lokhande <sayalil@codeaurora.org>
2016-11-18 16:05:50 +0530

    mmc: sdhci-msm: Disable controller clocks in suspend
    Generally, during card suspend we call mmc_power_off
    and disable controller clocks.
    Now consider below sequence of events :
    suspend -> resume -> suspend.
    1) During first platform suspend, mmc_power_off will
    be called and clocks would be disabled.
    2) As a part of platform resume, we enable controller
    clocks and defer card resume when
    MMC_BUSRESUME_NEEDS_RESUME flag is set.
    3) During next suspend we check if card is already
    suspended (i.e MMC_BUSRESUME_NEEDS_RESUME is set)
    and return without doing actual suspend (where we
    call mmc_power_off and disable controller clocks).
    
    So in this scenario, controller clocks will remain ON
    even though card state is SUSPENDED.
    
    Fix this by disabling controller clocks during suspend
    if controller clock was ON.
    
    CRs-Fixed: 1088893
    Change-Id: Id54a15d7f6a7131dab609eec1db158c64ada83ce
    Signed-off-by: Sayali Lokhande <sayalil@codeaurora.org>

--- drivers/mmc/host/sdhci-msm.c
+++ drivers/mmc/host/sdhci-msm.c
@@ -2616,7 +2616,22 @@ out:
 	return rc;
 }
 
+static void sdhci_msm_disable_controller_clock(struct sdhci_host *host)
+{
+	struct sdhci_pltfm_host *pltfm_host = sdhci_priv(host);
+	struct sdhci_msm_host *msm_host = pltfm_host->priv;
 
+	if (atomic_read(&msm_host->controller_clock)) {
+		if (!IS_ERR(msm_host->clk))
+			clk_disable_unprepare(msm_host->clk);
+		if (!IS_ERR(msm_host->pclk))
+			clk_disable_unprepare(msm_host->pclk);
+		sdhci_msm_bus_voting(host, 0);
+		atomic_set(&msm_host->controller_clock, 0);
+		pr_debug("%s: %s: disabled controller clock\n",
+			mmc_hostname(host->mmc), __func__);
+	}
+}
 
 static int sdhci_msm_prepare_clocks(struct sdhci_host *host, bool enable)
 {
@@ -3820,7 +3835,6 @@ static int sdhci_msm_suspend(struct device *dev)
 	struct sdhci_pltfm_host *pltfm_host = sdhci_priv(host);
 	struct sdhci_msm_host *msm_host = pltfm_host->priv;
 #endif
-	int ret = 0;
 
 #ifndef CONFIG_MMC_BLOCK_DEFERRED_RESUME
 	if (gpio_is_valid(msm_host->pdata->status_gpio))
@@ -3833,9 +3847,10 @@ static int sdhci_msm_suspend(struct device *dev)
 		goto out;
 	}
 
-	return sdhci_msm_runtime_suspend(dev);
+	sdhci_msm_runtime_suspend(dev);
 out:
-	return ret;
+	sdhci_msm_disable_controller_clock(host);
+	return 0;
 }
 
 static int sdhci_msm_resume(struct device *dev)
