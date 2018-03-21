3ad2fb906b9a919ffb3f4e07e769dbbb8733ae8b
GreyLeshy <andreiskull@gmail.com>
2018-03-21 19:03:03 +0300

    power: BCL: hide redundant logs

--- drivers/power/battery_current_limit.c
+++ drivers/power/battery_current_limit.c
@@ -255,7 +255,7 @@ static void __ref bcl_handle_hotplug(struct work_struct *work)
 				continue;
 			ret = cpu_down(_cpu);
 			if (ret)
-				pr_err("Error %d offlining core %d\n",
+				pr_debug("Error %d offlining core %d\n",
 					ret, _cpu);
 			else
 				pr_debug("Set Offline CPU:%d\n", _cpu);
@@ -264,7 +264,7 @@ static void __ref bcl_handle_hotplug(struct work_struct *work)
 				continue;
 			ret = cpu_up(_cpu);
 			if (ret)
-				pr_err("Error %d onlining core %d\n",
+				pr_debug("Error %d onlining core %d\n",
 					ret, _cpu);
 			else
 				pr_debug("Allow Online CPU:%d\n", _cpu);
