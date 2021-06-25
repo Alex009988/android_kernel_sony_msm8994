LINBIT/drbd

drivers/block: Use RCU_INIT_POINTER(x, NULL) in drbd/drbd_state.c
This patch replaces rcu_assign_pointer(x, NULL) with RCU_INIT_POINTER(x, NULL)

The rcu_assign_pointer() ensures that the initialization of a structure
is carried out before storing a pointer to that structure.
And in the case of the NULL pointer, there is no structure to initialize.
So, rcu_assign_pointer(p, NULL) can be safely converted to RCU_INIT_POINTER(p, NULL)

Signed-off-by: Monam Agarwal <monamagarwal123@gmail.com>
Signed-off-by: Lars Ellenberg <lars.ellenberg@linbit.com>

drbd/drbd_state.c
@@ -1576,7 +1576,7 @@ static int w_after_conn_state_ch(struct drbd_work *w, int unused)
 		old_conf = connection->net_conf;
 		connection->my_addr_len = 0;
 		connection->peer_addr_len = 0;
-		rcu_assign_pointer(connection->net_conf, NULL);
+		RCU_INIT_POINTER(connection->net_conf, NULL);
 		conn_free_crypto(connection);
 		mutex_unlock(&connection->resource->conf_update);
 

Alex009988/android_kernel_sony_msm8994

Signed-off-by: Andrey Cherepkov <andreiskull@gmail.com>

drivers/block/drbd/drbd_state.c
@@ -1545,7 +1545,7 @@ static int w_after_conn_state_ch(struct drbd_work *w, int unused)
 		old_conf = tconn->net_conf;
 		tconn->my_addr_len = 0;
 		tconn->peer_addr_len = 0;
-		rcu_assign_pointer(tconn->net_conf, NULL);
+		RCU_INIT_POINTER(tconn->net_conf, NULL);
 		conn_free_crypto(tconn);
 		mutex_unlock(&tconn->conf_update);
 