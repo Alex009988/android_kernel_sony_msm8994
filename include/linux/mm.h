a8ae2c6462dbe7b363cfa5055d147d719debff80
Sabar <31942277+sabarop@users.noreply.github.com>
2021-07-05 08:44:32 +0700

    Revert "fs/mpage.c: factor page_endio() out of mpage_end_io()"
    This reverts commit 619a854b4d3b7437d003d340d859d2616a4dfa05.

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -1647,6 +1647,11 @@ void page_cache_async_readahead(struct address_space *mapping,
 				pgoff_t offset,
 				unsigned long size);
 
+unsigned long max_sane_readahead(unsigned long nr);
+unsigned long ra_submit(struct file_ra_state *ra,
+			struct address_space *mapping,
+			struct file *filp);
+
 extern unsigned long stack_guard_gap;
 /* Generic expand stack which grows the stack according to GROWS{UP,DOWN} */
 extern int expand_stack(struct vm_area_struct *vma, unsigned long address);
