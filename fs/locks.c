f2d390211b77d0f9c1bd5e9994e15eb9b4f90c52
Sabar <31942277+sabarop@users.noreply.github.com>
2021-07-06 11:28:13 +0700

    Revert "locks: require that flock->l_pid be set to 0 for file-private locks"
    This reverts commit ae7a7012f1a73f92bbc4ff7831de1e6e3f5aaa82.

--- fs/locks.c
+++ fs/locks.c
@@ -1930,10 +1930,6 @@ int fcntl_getlk(struct file *filp, unsigned int cmd, struct flock __user *l)
 		goto out;
 
 	if (cmd == F_GETLKP) {
-		error = -EINVAL;
-		if (flock.l_pid != 0)
-			goto out;
-
 		cmd = F_GETLK;
 		file_lock.fl_flags |= FL_FILE_PVT;
 		file_lock.fl_owner = (fl_owner_t)filp;
@@ -2064,19 +2060,11 @@ int fcntl_setlk(unsigned int fd, struct file *filp, unsigned int cmd,
 	 */
 	switch (cmd) {
 	case F_SETLKP:
-		error = -EINVAL;
-		if (flock.l_pid != 0)
-			goto out;
-
 		cmd = F_SETLK;
 		file_lock->fl_flags |= FL_FILE_PVT;
 		file_lock->fl_owner = (fl_owner_t)filp;
 		break;
 	case F_SETLKPW:
-		error = -EINVAL;
-		if (flock.l_pid != 0)
-			goto out;
-
 		cmd = F_SETLKW;
 		file_lock->fl_flags |= FL_FILE_PVT;
 		file_lock->fl_owner = (fl_owner_t)filp;
@@ -2134,10 +2122,6 @@ int fcntl_getlk64(struct file *filp, unsigned int cmd, struct flock64 __user *l)
 		goto out;
 
 	if (cmd == F_GETLKP) {
-		error = -EINVAL;
-		if (flock.l_pid != 0)
-			goto out;
-
 		cmd = F_GETLK64;
 		file_lock.fl_flags |= FL_FILE_PVT;
 		file_lock.fl_owner = (fl_owner_t)filp;
@@ -2201,19 +2185,11 @@ int fcntl_setlk64(unsigned int fd, struct file *filp, unsigned int cmd,
 	 */
 	switch (cmd) {
 	case F_SETLKP:
-		error = -EINVAL;
-		if (flock.l_pid != 0)
-			goto out;
-
 		cmd = F_SETLK64;
 		file_lock->fl_flags |= FL_FILE_PVT;
 		file_lock->fl_owner = (fl_owner_t)filp;
 		break;
 	case F_SETLKPW:
-		error = -EINVAL;
-		if (flock.l_pid != 0)
-			goto out;
-
 		cmd = F_SETLKW64;
 		file_lock->fl_flags |= FL_FILE_PVT;
 		file_lock->fl_owner = (fl_owner_t)filp;
