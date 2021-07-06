a8fdf8f7461c2f8a609a008e13051b9d4b3cd4ea
Sabar <31942277+sabarop@users.noreply.github.com>
2021-07-06 11:30:05 +0700

    Revert "locks: remove "inline" qualifier from fl_link manipulation functions"
    This reverts commit 6f08717c38a91465c2bec067f1725109c43ac40e.

--- fs/locks.c
+++ fs/locks.c
@@ -511,7 +511,8 @@ static int posix_same_owner(struct file_lock *fl1, struct file_lock *fl2)
 }
 
 /* Must be called with the i_lock held! */
-static void locks_insert_global_locks(struct file_lock *fl)
+static inline void
+locks_insert_global_locks(struct file_lock *fl)
 {
 	lg_local_lock(&file_lock_lglock);
 	fl->fl_link_cpu = smp_processor_id();
@@ -520,7 +521,8 @@ static void locks_insert_global_locks(struct file_lock *fl)
 }
 
 /* Must be called with the i_lock held! */
-static void locks_delete_global_locks(struct file_lock *fl)
+static inline void
+locks_delete_global_locks(struct file_lock *fl)
 {
 	/*
 	 * Avoid taking lock if already unhashed. This is safe since this check
@@ -542,12 +544,14 @@ posix_owner_key(struct file_lock *fl)
 	return (unsigned long)fl->fl_owner;
 }
 
-static void locks_insert_global_blocked(struct file_lock *waiter)
+static inline void
+locks_insert_global_blocked(struct file_lock *waiter)
 {
 	hash_add(blocked_hash, &waiter->fl_link, posix_owner_key(waiter));
 }
 
-static void locks_delete_global_blocked(struct file_lock *waiter)
+static inline void
+locks_delete_global_blocked(struct file_lock *waiter)
 {
 	hash_del(&waiter->fl_link);
 }
